40d14163ba5bf9c1c75da8f68e60cc6383ac650d
Romeo Rizzi <romeo.rizzi@univr.it>
2025-03-29 06:41:18 +0100

    updated go script

--- strumenti/stuff/bin/goalgo.py
+++ strumenti/stuff/bin/goalgo.py
@@ -3,6 +3,9 @@ import os
 from sys import stderr, exit, argv
 import datetime
 
+os.system(f'date +"%Y-%m-%d_%B  %A%ttime: %H:%M" >> ~/.bin/go.log')
+os.system(f'echo " algo class" >> ~/.bin/go.log')
+os.system(f'echo >> ~/.bin/go.log')
 os.system(f'google-chrome --new-window https://etherpad.wikimedia.org/p/Algoritmi{str(datetime.date.today())[:4]}_indice_incontri')
 os.system(f'google-chrome https://etherpad.wikimedia.org/p/Algoritmi{str(datetime.date.today())}')
 os.system(f'google-chrome https://wbo.ophir.dev/boards/LavagnaALGO{str(datetime.date.today())}_1')
