0c862d346be30edec3eb6c64d509fe1634b18cd2
Romeo Rizzi <romeo.rizzi@univr.it>
2025-03-30 10:11:17 +0200

    sync the scripts

--- strumenti/stuff/bin/goalgo.ps1
+++ strumenti/stuff/bin/goalgo.ps1
@@ -1,12 +1,12 @@
 # Launch Google Chrome with specified URLs
-Start-Process "chrome.exe" -ArgumentList "--new-window","https://univr.zoom.us/j/94457731757"
-Start-Process "chrome.exe" -ArgumentList "https://rizzi.olinfo.it/algo"
-Start-Process "chrome.exe" -ArgumentList "https://wbo.ophir.dev/boards/LavagnaALGO$((Get-Date).ToString('yyyy-MM-dd'))_1"
-Start-Process "chrome.exe" -ArgumentList "https://etherpad.wikimedia.org/p/Algoritmi$((Get-Date).ToString('yyyy'))_indice_incontri"
-Start-Process "chrome.exe" -ArgumentList "https://etherpad.wikimedia.org/p/Algoritmi$((Get-Date).ToString('yyyy-MM-dd'))"
+Start-Process "chrome.exe" -ArgumentList "--new-window","https://wbo.ophir.dev/boards/algo$((Get-Date).ToString('yyyy-MM-dd'))_1"
+Start-Process "chrome.exe" -ArgumentList "https://etherpad.wikimedia.org/p/algo_meetings$((Get-Date).ToString('yyyy'))"
+Start-Process "chrome.exe" -ArgumentList "https://etherpad.wikimedia.org/p/algo$((Get-Date).ToString('yyyy-MM-dd'))"
+Start-Process "chrome.exe" -ArgumentList "https://univr.zoom.us/j/94457731757"
 
 # Display URLs
-Write-Host "Zoom Room ALGO URL: https://univr.zoom.us/j/94457731757"
-Write-Host "CMS algo URL: https://rizzi.olinfo.it/algo"
-Write-Host "Collaborative WhiteBoard(easy) URL: https://wbo.ophir.dev/boards/LavagnaALGO$((Get-Date).ToString('yyyy-MM-dd'))"
-Write-Host "Collaborative URL: https://etherpad.wikimedia.org/p/Algoritmi$((Get-Date).ToString('yyyy-MM-dd'))"
+Write-Host "URLs:"
+Write-Host " - Zoom algo Room: https://univr.zoom.us/j/94457731757"
+Write-Host "Etherpad algo index page: https://etherpad.wikimedia.org/p/algo_meetings$((Get-Date).ToString('yyyy'))"
+Write-Host " - Etherpad meeting report: https://etherpad.wikimedia.org/p/algo$((Get-Date).ToString('yyyy-MM-dd'))"
+Write-Host " - Collaborative WhiteBoard: https://wbo.ophir.dev/boards/algo$((Get-Date).ToString('yyyy-MM-dd'))_1"
